b7e9f000c00f9d058de9525c155e1e1dd5cbebc6
Cody Graham <codydgraham@gmail.com>
2018-07-31 20:05:47 -0400

    Created heading class to store useful heading variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Heading.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Heading.java
@@ -1,11 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 
-//import com.qualcomm.hardware.bosch
+import com.qualcomm.hardware.bosch.BNO055IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 public class Heading {
     private static float fieldOffset;
+    private static BNO055IMU imu;
 
     private float relativeOffset;
+
+    Heading(float relativeOffset) {
+        this.relativeOffset = relativeOffset;
+    }
 
     public static float getFieldOffset(){
         return fieldOffset;
@@ -14,11 +24,32 @@ public class Heading {
         Heading.fieldOffset = fieldOffset;
     }
     public static float getAbsoluteHeading(){
-        return 0f;
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        return errorCorrecter(angles.firstAngle);
+    }
+    public static float getFieldHeading(){
+        return errorCorrecter(getAbsoluteHeading() + fieldOffset);
+    }
+    private static float errorCorrecter(float sumOfHeadings){
+        if (-180f > sumOfHeadings){
+            sumOfHeadings += 360f;
+        }
+        else if (sumOfHeadings > 180f){
+            sumOfHeadings -= 180f;
+        }
+        return sumOfHeadings;
+    }
+    public static Heading createFieldHeading(){
+        
     }
 
     public void setRelativeOffset(float relativeOffset){
         this.relativeOffset = relativeOffset;
     }
-
+    public float getRelativeOffset(){
+        return relativeOffset;
+    }
+    public float getRelativeHeading(){
+        return errorCorrecter(getFieldHeading() + relativeOffset);
+    }
 }
