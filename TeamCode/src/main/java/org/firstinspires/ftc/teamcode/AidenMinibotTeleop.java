7643117cf6e323eb12bbc443c979a68d4adf89a5
Zoe Lambert <zsslambert@gmail.com>
2018-08-02 19:03:38 -0400

    Created Teleop Program For Minibot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AidenMinibotTeleop.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "My1Program",group = "")
+public class AidenMinibotTeleop extends OpMode {
+    DcMotor motorL;
+    DcMotor motorR;
+    @Override
+    public void init() {
+        motorL = hardwareMap.get(DcMotor.class,"left;");
+        motorR = hardwareMap.get(DcMotor.class,"right;");
+
+        motorL.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorR.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        motorL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    @Override
+    public void loop() {
+        motorL.setPower(-gamepad1.left_stick_y);
+        motorR.setPower(-gamepad1.right_stick_y);
+    }
+}
