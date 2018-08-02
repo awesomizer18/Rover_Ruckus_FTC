c63307dd79d63199a6bbe28b5f529bb63ea12a97
Cody Graham <codydgraham@gmail.com>
2018-08-02 19:06:23 -0400

    MiniBot Teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/andrewMinibotTeleop.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "Andrew.Minibot", group = "")
+public class andrewMinibotTeleop extends OpMode{
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+    @Override
+    public void init() {
+        leftMotor = hardwareMap.get(DcMotor.class,"left");
+        rightMotor = hardwareMap.get(DcMotor.class,"right");
+
+        leftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    @Override
+    public void loop() {
+        leftMotor.setPower(-gamepad1.left_stick_y);
+        rightMotor.setPower(gamepad1.right_stick_y);
+
+    }
+}
