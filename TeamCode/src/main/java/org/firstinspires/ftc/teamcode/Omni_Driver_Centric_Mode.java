efc1e816a157be356aff813f17c120f04a5a294a
FTC 12888 <ftc.12888@gmail.com>
2018-08-14 19:58:36 -0400

    Setting up Driver_Cetric_Mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Omni_Driver_Centric_Mode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Omni_Driver_Centric_Mode.java
@@ -1,4 +1,45 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Omni_Driver_Centric_Mode {
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Driver Centric Mode Test", group="Competition")//Competition/Main
+public class Omni_Driver_Centric_Mode extends Omni_Parent
+{
+    private double correctionValueInDegrees;
+
+    @Override
+    public void initializeRobot() {
+        correctionValueInDegrees = calculateHeading();
+    }
+
+    public void cycle() {
+        double leftPower;
+        double rightPower;
+
+
+        //leftPower = -controllerThreshold(gamepad1.left_stick_y);
+        //rightPower = -controllerThreshold(gamepad1.right_stick_y);
+
+        double away = -controllerThreshold(gamepad1.left_stick_y);
+        double side  = controllerThreshold(gamepad1.left_stick_x);
+        double turn  =  controllerThreshold(gamepad1.right_stick_x);
+        double directionOfJoystick = Math.toDegrees(Math.atan2(side, away));
+
+        double heading = -1 * calculateHeading();
+        double robotOrientedCommand = directionOfJoystick - heading;
+
+        double inputSize = Math.sqrt(Math.pow(away, 2) + Math.pow(side, 2));
+        double strafe = inputSize * Math.sin(Math.toRadians(robotOrientedCommand));
+        double forward =  inputSize * Math.cos(Math.toRadians(robotOrientedCommand));
+
+        setDrive(forward, turn, strafe);
+
+        telemetry.addData("directionOfJoystick",directionOfJoystick);
+        telemetry.addData("robotOrientedCommand", robotOrientedCommand);
+        telemetry.addData("Heading", heading);
+        telemetry.addData("Stafe", strafe);
+        telemetry.addData("Forward", forward);
+        telemetry.update();
+
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Omni_Parent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Omni_Parent.java
@@ -1,22 +1,35 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+
+
 
 public class Omni_Parent extends LinearOpMode {
     protected DcMotor frontRightDrive = null;
     protected DcMotor frontLeftDrive = null;
     protected DcMotor backRightDrive = null;
     protected DcMotor backLeftDrive = null;
+    protected BNO055IMU imu = null;
+
     private final double JOYSTICK_THRESHOLD = 0.1;
+    private float zeroFactor = 0.0f;
 
 
     @Override
-    public void runOpMode(){
+    public void runOpMode() {
         frontLeftDrive = hardwareMap.get(DcMotor.class, "fl");
         frontRightDrive = hardwareMap.get(DcMotor.class, "fr");
         backLeftDrive = hardwareMap.get(DcMotor.class, "bl");
         backRightDrive = hardwareMap.get(DcMotor.class, "br");
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
 
         frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
@@ -28,12 +41,24 @@ public class Omni_Parent extends LinearOpMode {
         backRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        BNO055IMU.Parameters imuParameters = new BNO055IMU.Parameters();
+        imuParameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        imuParameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        imuParameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        imuParameters.loggingEnabled = true;
+        imuParameters.loggingTag = "IMU";
+        imuParameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu.initialize(imuParameters);
+
+
         initializeRobot();
 
         waitForStart();
 
         startRobot();
     }
+
     public void initializeRobot() {
 
     }
@@ -41,20 +66,39 @@ public class Omni_Parent extends LinearOpMode {
     public void startRobot() {
 
     }
+
     protected void setDrive(double forwardsPower, double turnPower, double strafePower) {
         frontRightDrive.setPower(forwardsPower - turnPower - strafePower);
         backRightDrive.setPower(forwardsPower - turnPower + strafePower);
         frontLeftDrive.setPower(forwardsPower + turnPower + strafePower);
         backLeftDrive.setPower(forwardsPower + turnPower - strafePower);
     }
-    protected double controllerThreshold(double number){
+
+    protected double controllerThreshold(double number) {
         if (Math.abs(number) <= JOYSTICK_THRESHOLD) {
             return 0.0;
-        }
-        else {
+        } else {
             return number;
         }
     }
 
+    protected float calculateHeading() {
+        try {
+            Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            float heading = angles.firstAngle - zeroFactor;
+            while (heading > 180.0f)
+                heading -= 360.0f;
+            while (heading < -180.0f)
+                heading += 360.0f;
+            return heading;
+        } catch (Exception e) {
+            telemetry.addLine("calculateHeading Failed.");
+            telemetry.update();
+            sleep(2000);
+            e.printStackTrace();
+            return 0.0f;
+        }
 
+
+    }
 }
