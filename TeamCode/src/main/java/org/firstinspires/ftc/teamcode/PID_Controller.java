9fc0eaf8fd476ee147ed806ca371a183fa2b2f12
FTC 12888 <ftc.12888@gmail.com>
2018-08-14 19:27:07 -0400

    Fixed todos in PID_Controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID_Controller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID_Controller.java
@@ -5,29 +5,25 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 /**
  * Created by user on 7/10/2018.
  */
-// TODO: When creating a PID_Controller object, it doesn't accept Gains like it used to
 public class PID_Controller{
     ElapsedTime runtime;
 
-    private double setpoint = 0;
-    private double error = 0;
-    private double lastError = 0;
-    private double time = 0;
-    private double lastTime = 0;
-    private double pValue = 0;
+
+    private double setpoint = 0.0;
+    private double error = 0.0;
+    private double lastError = 0.0;
+    private double time = 0.0;
+    private double lastTime = 0.0;
+    private double pValue = 0.0;
     private double PGAIN;
-    private double iValue = 0;
+    private double iValue = 0.0;
     private double IGAIN;
     private double dValue = 0.0;
     private double DGAIN;
-    private boolean isFirstTime = true;
-    // TODO: Either implement CONVERSION_RATE or remove it if the feature is forgotten.
-    private double CONVERSION_RATE;
 
-    public void PID_Loop(double PGAIN, double IGAIN, double DGAIN){
+    public PID_Controller(double PGAIN, double IGAIN, double DGAIN){
         this.runtime = new ElapsedTime();
-        // TODO: Make the constructor fully reset the controller
-        this.runtime.reset();
+        resetPID();
         this.PGAIN = PGAIN;
         this.IGAIN = IGAIN;
         this.DGAIN = DGAIN;
@@ -43,13 +39,7 @@ public class PID_Controller{
         time = runtime.seconds();
         pValue = PGAIN * error;
         iValue += IGAIN * (lastError + error) * (0.5) * (time - lastTime);
-        //dValue
         dValue = DGAIN * (error - lastError) / (time - lastTime);
-        if (isFirstTime)
-        {
-            iValue = 0.0;
-            isFirstTime = false;
-        }
         return pValue + iValue + dValue;
     }
 
@@ -59,7 +49,6 @@ public class PID_Controller{
 
     public void resetPID(double startingIValue) {
         runtime.reset();
-        isFirstTime = true;
         iValue = startingIValue;
     }
 
