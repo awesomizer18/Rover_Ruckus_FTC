f1233ee9ecd291a7fe73fde321587aa44e0c11cb
FLL_4 <FTC.12888@gmail.com>
2018-08-16 19:00:04 -0400

    Daniel - Added Autonomous program to run Figure 8

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lukes2_minibot_auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lukes2_minibot_auto.java
@@ -1,9 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="0")
 public class Lukes2_minibot_auto extends lukes_minibot_auto{
     @Override
 
     public void runAuto() {
-
+        setDrive(1.0,1.0);
+        sleep(1500);
+        setDrive(-1.0,1.0);
+        sleep(100);
+        setDrive(1,1);
+        sleep(750);
+        setDrive(-1,1);
+        sleep(100);
+        setDrive(1,1);
+        sleep(1500);
+        setDrive(-1,1);
+        sleep(100);
+        setDrive(1,1);
+        sleep(750);
+        setDrive(0,0);
     }
 }
