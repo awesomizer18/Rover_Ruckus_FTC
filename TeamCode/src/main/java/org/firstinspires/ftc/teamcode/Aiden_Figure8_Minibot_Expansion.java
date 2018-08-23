7ac5b0a15b27949c02208623541dc2d0c912d8ed
= <=>
2018-08-23 18:32:15 -0400

    fixed figure8 problem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Aiden_Figure8_Minibot_Expansion.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Aiden Auto Minibot Circle Figure-8",group = "")
+public class Aiden_Figure8_Minibot_Expansion extends AidenMinibotAuto {
+    @Override
+    public void runAuto() {
+       set
+
+
+    }
+}
