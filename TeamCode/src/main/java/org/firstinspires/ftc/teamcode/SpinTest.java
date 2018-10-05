2d981f09204d39f64234012d9a6527df50acbf26
Team 12888 <ftc.12888@gmail.com>
2018-10-04 21:12:52 -0400

    Added SpinTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpinTest.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "Spin_Test")
+public class SpinTest extends OpMode {
+
+    private DcMotor backLeftDrive;
+    private DcMotor backRightDrive;
+    private DcMotor frontLeftDrive;
+    private DcMotor frontRightDrive;
+    private PID_Controller turnPID = new PID_Controller(0.025, 0.0, 0.003);
+    Heading heading;
+    BNO055IMU imu;
+
+    @Override
+    public void init() {
+        backLeftDrive = hardwareMap.get(DcMotor.class, "bld");
+        backRightDrive = hardwareMap.get(DcMotor.class, "brd");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "fld");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "frd");
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        backLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        setupImu();
+        Heading.setImu(imu);
+        Heading.setFieldOffset(0.0f);
+    }
+
+    @Override
+    public void start() {
+        heading = Heading.createRelativeHeading(0.0f);
+
+        turnPID.resetPID();
+        turnPID.setSetpoint(heading.getRelativeHeading());
+    }
+
+    @Override
+    public void loop() {
+        double turnPower = turnPID.update(heading.getRelativeHeading());
+       setDrive(0.0, turnPower, 0.0);
+    }
+
+    private void setDrive(double forwardPower, double turnPower, double strafePower) {
+        backLeftDrive.setPower(forwardPower + turnPower - strafePower);
+        backRightDrive.setPower(forwardPower - turnPower + strafePower);
+        frontLeftDrive.setPower(forwardPower + turnPower + strafePower);
+        frontRightDrive.setPower(forwardPower - turnPower - strafePower);
+    }
+    private void setupImu() {
+        BNO055IMU.Parameters imuParameters = new BNO055IMU.Parameters();
+        imuParameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        imuParameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        imuParameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        imuParameters.loggingEnabled = true;
+        imuParameters.loggingTag = "IMU";
+        imuParameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu.initialize(imuParameters);
+    }
+}
+
