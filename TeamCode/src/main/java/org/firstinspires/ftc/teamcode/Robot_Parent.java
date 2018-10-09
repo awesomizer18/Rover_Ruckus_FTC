75300a72f42ab48f4e19c876dfe437df53bf9d10
Team 12888 <ftc.12888@gmail.com>
2018-10-09 19:29:11 -0400

    Changed from private to protected

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot_Parent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot_Parent.java
@@ -3,21 +3,33 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-public class Robot_Parent extends LinearOpMode {
+public abstract class Robot_Parent extends LinearOpMode {
 
-    private final double EC_PER_DEGREE = 20.04;
-    private DcMotor backLeftDrive;
-    private DcMotor backRightDrive;
-    private DcMotor frontLeftDrive;
-    private DcMotor frontRightDrive;
+    protected final double EC_PER_DEGREE = 20.04;
+    protected DcMotor backLeftDrive;
+    protected DcMotor backRightDrive;
+    protected DcMotor frontLeftDrive;
+    protected DcMotor frontRightDrive;
 
     @Override
     public void runOpMode() throws InterruptedException {
-
     }
-    private void setDrive(double forwardPower, double turnPower, double strafePower) {
+
+    abstract public void getReady();
+
+    abstract public void go();
+
+    protected void setDrive(double forwardPower, double turnPower, double strafePower) {
         backLeftDrive.setPower(forwardPower + turnPower - strafePower);
         backRightDrive.setPower(forwardPower - turnPower + strafePower);
         frontLeftDrive.setPower(forwardPower + turnPower + strafePower);
         frontRightDrive.setPower(forwardPower - turnPower - strafePower);
+    }
+    protected double getTurnPosition() {
+        double position = backLeftDrive.getCurrentPosition() - backRightDrive.getCurrentPosition()
+                + frontLeftDrive.getCurrentPosition() - frontRightDrive.getCurrentPosition();
+        position /= 4.0;
+        position /= EC_PER_DEGREE;
+        return position;
+    }
 }
