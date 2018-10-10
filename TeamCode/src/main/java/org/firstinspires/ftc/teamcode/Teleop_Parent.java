454d78d6199ba87a8790979f6ea8d5116863223f
Cody Graham <codydgraham@gmail.com>
2018-10-09 20:14:49 -0400

    created teleop_Parent

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_Parent.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+public abstract class Teleop_Parent extends Robot_Parent {
+    private PID_Controller holdTurnPID = new PID_Controller(0.025, 0.0, 0.0);
+
+    @Override
+    public void getReady() {
+
+    }
+
+    @Override
+    public void go() {
+        begin();
+        while (opModeIsActive()) {
+            run();
+        }
+    }
+
+    abstract public void begin();
+
+    abstract public void run();
+
+    protected double mapJoyStick(float joyStickImput){
+        return (double)joyStickImput;
+    }
+}
