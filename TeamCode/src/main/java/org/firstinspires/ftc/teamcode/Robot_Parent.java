48416f522b78cbcb44e869fa74c35ca5911731db
Team 12888 <ftc.12888@gmail.com>
2018-10-11 19:18:32 -0400

    Added extra functionality to Robot_Parent class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot_Parent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot_Parent.java
@@ -1,12 +1,37 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public abstract class Robot_Parent extends LinearOpMode {
 
+    private final double ENCODER_COUNTS_PER_DEGREE = 18.06;
+
+    private DcMotor backLeftDrive;
+    private DcMotor backRightDrive;
+    private DcMotor frontLeftDrive;
+    private DcMotor frontRightDrive;
+
+    private PID_Controller goToTurnPID = new PID_Controller(0.025, 0.0, 0.0);
+    private PID_Controller holdTurnPID = new PID_Controller(0.0,0.0,0.0);
+
     @Override
     public void runOpMode() throws InterruptedException {
+        backLeftDrive = hardwareMap.get(DcMotor.class, "bld");
+        backRightDrive = hardwareMap.get(DcMotor.class, "brd");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "fld");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "frd");
+
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        backLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        backRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        frontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        frontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
         initialize();
         waitForStart();
         play();
@@ -14,4 +39,21 @@ public abstract class Robot_Parent extends LinearOpMode {
 
     public abstract void initialize();
     public abstract void play();
+
+    // Functions
+
+    private void setDrive(double forwardPower, double turnPower, double strafePower) {
+        backLeftDrive.setPower(forwardPower + turnPower - strafePower);
+        backRightDrive.setPower(forwardPower - turnPower + strafePower);
+        frontLeftDrive.setPower(forwardPower + turnPower + strafePower);
+        frontRightDrive.setPower(forwardPower - turnPower - strafePower);
+    }
+
+    private double getTurnPosition() {
+        double position = backLeftDrive.getCurrentPosition() + frontLeftDrive.getCurrentPosition()
+                - backRightDrive.getCurrentPosition() - frontRightDrive.getCurrentPosition();
+        position /= 4.0;
+        position /= ENCODER_COUNTS_PER_DEGREE;
+        return position;
+    }
 }
