21354a34c2e98569fa5c0296233a6518c2c74dab
Team 12888 <ftc.12888@gmail.com>
2018-10-16 19:36:32 -0400

    Added preferences

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tank_Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tank_Drive.java
@@ -12,8 +12,14 @@ public class Tank_Drive extends Teleop_Parent {
         double rightDrivePower = -gamepad1.left_stick_y;
         double leftStrafePower = gamepad1.left_stick_x;
         double rightStrafePower = gamepad1.right_stick_x;
+
+        setDrive(leftDrivePower, rightDrivePower, rightStrafePower, leftStrafePower);
     }
-
-    @Override
-
+    private void setDrive (double leftDrivePower, double rightDrivePower, double leftStrafePower, double rightStrafePower){
+        backLeftDrive.setPower(leftDrivePower - leftStrafePower);
+        backRightDrive.setPower(rightDrivePower + rightStrafePower);
+        frontLeftDrive.setPower(leftDrivePower + leftStrafePower);
+        frontRightDrive.setPower(rightDrivePower - rightStrafePower);
+    }
 }
+
