352a9a3ed2deb6181503ebcb1a77ecf37df5e126
Navigator365 <40570437+Navigator365@users.noreply.github.com>
2018-10-16 18:09:55 -0400

    created teleop test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_Test.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp (name = "Teleop_Test", group = "main")
+public class Teleop_Test extends Teleop_Parent {
+    @Override
+    public void run() {
+        double drivePower = gamepad1.left_stick_x;
+        double turnPower = gamepad1.right_stick_x;
+        double strafePower = gamepad1.right_stick_y;
+        setDrive(drivePower,turnPower, strafePower);
+    }
+
+    @Override
+    public void begin() {
+
+    }
+}
