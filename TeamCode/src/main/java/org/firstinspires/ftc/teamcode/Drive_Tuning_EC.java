e70f02ffce87892229b5d93f4fa71034f3d85466
Surface Book <ftc.00389@gmail.com>
2018-10-16 21:02:13 -0400

    Made robot drive forward 60 inches, then left 60 inches

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive_Tuning_EC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive_Tuning_EC.java
@@ -2,17 +2,33 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name = "Encoder Measuring Code", group = "A")
+@Autonomous(name = "PID Test", group = "A")
 public class Drive_Tuning_EC extends Autonomous_Parent {
 
     @Override
     public void go() {
         super.go();
+        holdTurnPID.setSetpoint(getTurnPosition());
         forwardPID.setSetpoint(getForwardPosition() + 60.0);
-        while (opModeIsActive())
+        for (int i = 0; i < 200; i++)
         {
             double forwardPower = forwardPID.update(getForwardPosition());
-            setDrive(forwardPower, 0.0, 0.0);
+            double turnPower = holdTurnPID.update(getTurnPosition());
+            setDrive(forwardPower, turnPower, 0.0);
+            sleep(15);
         }
+
+        setDrive(0.0, 0.0, 0.0);
+        sleep(1000);
+
+        strafePID.setSetpoint(getStrafePosition() - 60.0);
+        for (int i = 0; i < 200; i++)
+        {
+            double strafePower = strafePID.update(getStrafePosition());
+            double turnPower = holdTurnPID.update(getTurnPosition());
+            setDrive(0.0, turnPower, strafePower);
+            sleep(15);
+        }
+        setDrive(0.0, 0.0, 0.0);
     }
 }
