29ee4d4065389e6abccc1f58b81e2be192ad970c
Tyler Gunther <tylgmd@gmail.com>
2018-10-25 19:15:15 -0500

    removed getTurnPosition and added heading to replace it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot_Parent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot_Parent.java
@@ -1,12 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public abstract class Robot_Parent extends LinearOpMode {
-
-    // TODO: remember to set values to diff number
-    private final double ENCODER_COUNTS_PER_DEGREE = 18.06;
 
     protected DcMotor backLeftDrive;
     protected DcMotor backRightDrive;
@@ -16,12 +15,16 @@ public abstract class Robot_Parent extends LinearOpMode {
     protected PID_Controller goToTurnPID = new PID_Controller(0.025, 0.0, 0.0);
     protected PID_Controller holdTurnPID = new PID_Controller(0.0,0.0,0.0);
 
+    private BNO055IMU imu;
+
     @Override
     public void runOpMode() throws InterruptedException {
         backLeftDrive = hardwareMap.get(DcMotor.class, "bld");
         backRightDrive = hardwareMap.get(DcMotor.class, "brd");
         frontLeftDrive = hardwareMap.get(DcMotor.class, "fld");
         frontRightDrive = hardwareMap.get(DcMotor.class, "frd");
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
 
         backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         backRightDrive.setDirection(DcMotor.Direction.FORWARD);
@@ -32,6 +35,11 @@ public abstract class Robot_Parent extends LinearOpMode {
         backRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         frontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         frontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        setupImu();
+
+        Heading.setImu(imu);
+        Heading.setFieldOffset(-Heading.getAbsoluteHeading());
 
         initialize();
         telemetry.addLine("Initialization Complete");
@@ -55,10 +63,18 @@ public abstract class Robot_Parent extends LinearOpMode {
     }
 
     protected double getTurnPosition() {
-        double position = backLeftDrive.getCurrentPosition() + frontLeftDrive.getCurrentPosition()
-                - backRightDrive.getCurrentPosition() - frontRightDrive.getCurrentPosition();
-        position /= 4.0;
-        position /= ENCODER_COUNTS_PER_DEGREE;
-        return position;
+        return Heading.getFieldHeading();
+    }
+
+    private void setupImu() {
+        BNO055IMU.Parameters imuParameters = new BNO055IMU.Parameters();
+        imuParameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        imuParameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        imuParameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        imuParameters.loggingEnabled = true;
+        imuParameters.loggingTag = "IMU";
+        imuParameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu.initialize(imuParameters);
     }
 }
